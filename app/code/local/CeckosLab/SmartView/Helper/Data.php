6f0d0f21d7c521442fb5900bf633aaa1e8dd1d42
aaronthorp <aaron@thinksoftware.net.au>
2014-05-28 13:11:03 +1000

    initial commit

---
+++ app/code/local/CeckosLab/SmartView/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+class CeckosLab_SmartView_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    protected $_cartProductIds;
+
+
+    public function __construct() {
+        $product_ids = array();
+        $products = Mage::getSingleton('checkout/session')->getQuote()->getAllVisibleItems();
+        foreach ($products as $_products){
+            $product_ids [$_products->getProductId()] = $_products->getQty();
+        }
+        $this->_cartProductIds = $product_ids;
+    }
+    
+    public function isInCart($product) {
+        return array_key_exists($product->getId(), $this->_cartProductIds); 
+    }
+    
+    public function getQty($product) {
+        return  $this->_cartProductIds [$product->getId()]; 
+    }
+}
